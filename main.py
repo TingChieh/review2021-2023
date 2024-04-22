34b5b4b98d15110c4d4d9c242924e9cd6d117d77
TingChieh <tingchieh77@outlook.com>
2024-04-22 09:00:15 +0800

    txt -> md

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import re
 import requests
 from tqdm import tqdm
 
-with open('2022.md', encoding="utf-8") as f:
+with open('2021.md', encoding="utf-8") as f:
     metadata = f.read()
     
     links = []
@@ -17,7 +17,12 @@ with open('2022.md', encoding="utf-8") as f:
     
 count = 0
     
-with open('output_2022.txt', 'w', encoding="utf-8") as output:
+with open('output_2021.md', 'w', encoding="utf-8") as output:
+    
+    output.write("| 链接 | 状态 |\n")
+    output.write("| ---- | ---- |\n")
+    output.flush()
+    
     for link in tqdm(links):
         r = None
         for i in range(3):
@@ -25,17 +30,17 @@ with open('output_2022.txt', 'w', encoding="utf-8") as output:
                 r = requests.get(link, timeout=15)
                 break
             except Exception as e:
-                print(link, "   ---FAILED")
-                output.write(link + '   ---FAILED\n')
+                print("| " + link, " | FAILED |")
+                output.write("| " + link + ' | FAILED |\n')
                 output.flush()
         if r is None:
-            print(link, "---FAILED")
-            output.write(link + '   ---FAILED\n')
+            print("| " + link, " | FAILED |")
+            output.write(link + ' | FAILED |\n')
             output.flush()
             continue
         if r.status_code in [200, 403]:
             count += 1
-            print(link, "   ---OK")
-            output.write(link + '   ---OK\n')
+            print("| " + link, " | OK |")
+            output.write("| " + link + ' | OK |\n')
             output.flush()
 print(count)
